b2a0617fc5587af233dc5439ea0d4b5556645141
jakhongiromonboev <jakhongiromonboev@gmail.com>
2025-10-12 01:36:41 +0900

    feat: develop verifyAuth middleware logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,6 +1,11 @@
-import { LoginInput, Member, MemberInput } from "../libs/types/member";
+import {
+  ExtendedRequest,
+  LoginInput,
+  Member,
+  MemberInput,
+} from "../libs/types/member";
 import { T } from "../libs/types/common";
-import { request, Request, Response } from "express";
+import { NextFunction, request, Request, Response } from "express";
 import MemberService from "../models/Member.service";
 import Errors, { HttpCode } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
@@ -50,6 +55,22 @@ memberController.login = async (req: Request, res: Response) => {
     res.status(HttpCode.OK).json({ member: result, accesToken: token });
   } catch (err) {
     console.log("Error,login", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+memberController.verifyAuth = async (
+  req: ExtendedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    console.log("verifyAuth");
+    const token = req.cookies["acessToken"];
+    if (token) req.member = await authService.checkAuth(token);
+  } catch (err) {
+    console.log("Error, verifyAuth", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
   }
--- src/models/Auth.service.ts
+++ src/models/Auth.service.ts
@@ -6,7 +6,7 @@ import Errors, { HttpCode, Message } from "../libs/Errors";
 class AuthService {
   private readonly secretToken;
   constructor() {
-    this.secretToken = process.env.SECRET_TOKEN;
+    this.secretToken = process.env.SECRET_TOKEN as string;
   }
 
   public async createToken(payload: Member) {
@@ -27,6 +27,15 @@ class AuthService {
         }
       );
     });
+  }
+
+  public async checkAuth(token: string): Promise<Member> {
+    const result: Member = (await jwt.verify(
+      token,
+      this.secretToken
+    )) as Member;
+    console.log(`-----[AUTH] memberNick: ${result.memberNick} ----`);
+    return result;
   }
 }
 
